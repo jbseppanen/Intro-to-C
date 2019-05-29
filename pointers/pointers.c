e0a8544b3c51a6f0347d8c2c344a594430b98c32
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-28 19:57:40 -0700

    Pointers.c now passes all tests.

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -15,7 +15,13 @@
 */
 void string_copy(char *x, char *y)
 {
-
+    int counter = 0;
+    while (*(y + counter) != '\0')
+    {
+        x[counter] = *(y + counter);
+        counter++;
+    }
+    x[counter] = '\0';
 }
 
 /*
@@ -26,9 +32,21 @@ void string_copy(char *x, char *y)
 
     Do not use the `strchr` function from the standard library.
 */
-char *find_char(char *str, int c)
+char *find_char(char *str, char c)
 {
-
+    int counter = 0;
+    while (*(str + counter) != '\0')
+    {
+        if (*(str + counter) == c)
+        {
+            return (str + counter);
+        }
+        else
+        {
+            counter++;
+        }
+    }
+    return 0;
 }
 
 /*
@@ -41,17 +59,55 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
+    // char *ptr;
+    int counter1 = 0;
+    while (*(haystack + counter1) != '\0')
+    {
+        // printf("Outer While Haystack Char %c\n", *(haystack + counter1));
+        if (*(haystack + counter1) == *needle)
+        {
+            int counter2 = 0;
+            int position = counter1;
+            while (*(haystack + counter1) != '\0')
+            {
+                // printf("Inner While Haystack Char %c\n", *(haystack + counter1));
+                // printf("Inner While Needle Char %c\n", *(needle + counter2));
 
+                if (*(haystack + counter1) != *(needle + counter2))
+                {
+                    break;
+                }
+                else if (*(needle + counter2+1) == '\0')
+                {
+                    // printf("Found It!\n");
+                    return (haystack + position);
+                }
+                counter1++;
+                counter2++;
+            }
+        }
+        counter1++;
+    }
+    return 0;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *found_char = find_char("hello", 'e');
-    char *found_string = find_string("world", "or");
+    // char buffer[1024];
 
+    // string_copy(buffer, "Hello!");
+    // printf("%s\n", buffer); // Prints "Hello!"
+
+    char *found_char = find_char("hello", 'e');
     printf("Found char: %s\n", found_char);
+
+    char *found_string = find_string("world", "or");
     printf("Found string: %s\n", found_string);
+
+    // char *str = "LambdaSchool";
+    // char *found = find_string("LambdaSchool", "School");
+    // printf("Found string: %s\n", found);
 
     return 0;
 }
